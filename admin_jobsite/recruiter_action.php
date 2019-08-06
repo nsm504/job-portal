bbc08082d12aee7bcb20af9f93292512ac8f19c9
nsm504 <nirmalmenon97@gmail.com>
2019-08-06 13:55:13 +0530

    Update recruiter_action.php

--- admin_jobsite/recruiter_action.php
+++ admin_jobsite/recruiter_action.php
@@ -21,7 +21,15 @@ if(isset($_POST['user_id']))
 	$row= mysqli_fetch_array($result2);
 
 	try{
-
+		//Server settings
+		/*$mail->SMTPDebug = 1;                                       // Enable verbose debug output
+		$mail->isSMTP();                                            // Set mailer to use SMTP
+		$mail->Host       = 'smtp.sendgrid.net';                    // Specify main and backup SMTP servers
+		$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+		$mail->Username   = 'user@example.com';                     // SMTP username
+		$mail->Password   = 'secret';                               // SMTP password
+		$mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+		$mail->Port       = 587;            */                        // TCP port to connect to
 		$mail->setFrom('proactivecba@gmail.com','proactivecba');
 		$mail->addAddress($row["recruiter_email"],$row["recruiter_first"]);
 
