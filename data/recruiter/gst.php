c21a3f864fb5b70ad6060e0d76eb4f7ea6b74425
Deva D <nirmalmenon97@gmail.com>
2019-08-06 11:24:22 +0530

    Add files via upload

---
+++ data/recruiter/gst.php
@@ -0,0 +1,17 @@
+<?php
+						if(isset($_SESSION['r_id'])){
+ 						$SESSION = $_SESSION['r_id'];
+
+						$sql= "SELECT gst  FROM recruiter where recruiter_id='$SESSION'";
+						$result = mysqli_query($conn,$sql);
+
+ 						if($result->num_rows>0){
+							while($row=$result->fetch_assoc())
+							{
+						  	echo $row["gst"]; 
+							}
+					 	}
+					}
+
+
+ ?>
---
+++ data/recruiter/mobile.php
@@ -0,0 +1,17 @@
+<?php
+						if(isset($_SESSION['r_id'])){
+ 						$SESSION = $_SESSION['r_id'];
+
+						$sql= "SELECT mobileno FROM recruiter where recruiter_id='$SESSION'";
+						$result = mysqli_query($conn,$sql);
+
+ 						if($result->num_rows>0){
+							while($row=$result->fetch_assoc())
+							{
+						  	echo $row["mobileno"]; 
+							}
+					 	}
+					}
+
+
+ ?>
